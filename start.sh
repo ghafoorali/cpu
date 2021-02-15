335254c7570d93d16a7b9a408f2cb4b897346636
ghafoorali <79002732+ghafoorali@users.noreply.github.com>
2021-02-15 13:41:37 +0500

    Update start.sh
    new setting

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -a cpupower -o stratum+tcp://cpupower.sea.mine.zpool.ca:6240 -u DGBeQ4sAoq7PuyKYYKNSM2PtMpUwDFm67i -p c=DOGE
+./cpuminer-sse2 -a cpupower -o stratum+tcp://cpupower.asia.mine.zergpool.com:4250 -u qr9e33jg396x25wv2h7rx6t3t8tlqyu89u0mx9lljf -p  c=BCH
 
 
 done
